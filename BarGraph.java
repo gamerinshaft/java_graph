03103bc6621a91c6661be51811932427c3cf211c
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-28 13:56:17 +0900

    背景色をグレーに変更した

--- BarGraph.java
+++ BarGraph.java
@@ -5,7 +5,7 @@ public class BarGraph extends Applet {
   int pa[]=new int[12];
 
   public void init() {
-     setBackground(new Color(255,255,255));
+     setBackground(new Color(25,25,25));
      pa[0]=180;pa[1]=120;pa[2]=83;pa[3]=112;pa[4]=90;pa[5]=109;
      pa[6]=120;pa[7]=130;pa[8]=200;pa[9]=155;pa[10]=180;pa[11]=170;
    }
