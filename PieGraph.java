bd0d14ee90bc344ceb2a6e33a5a7d1a3a8855429
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-30 03:14:18 +0900

    メーターのグラデーション

--- PieGraph.java
+++ PieGraph.java
@@ -4,13 +4,15 @@ import java.awt.*;
 public class PieGraph extends Applet {
   float pa[]=new float[8];
   float max = 0;
+  float sum = 0;
   public void init() {
      setBackground(new Color(25,25,25));
-     pa[0]=100;pa[1]=640;pa[2]=340;pa[3]=412;pa[4]=560;pa[5]=109;
-     pa[6]=800;pa[7]=250;
+     pa[0]=20;pa[1]=40;pa[2]=20;pa[3]=80;pa[4]=60;pa[5]=100;
+     pa[6]=40;pa[7]=50;
      //値の比較
      int i;
-     for(i=0;i<pa.length;i++){
+     for(i=0;i<8;i++){
+        sum += pa[i];
         if(max<=pa[i]){
           max = pa[i];
         }
@@ -18,10 +20,10 @@ public class PieGraph extends Applet {
 
    }
   public void paint(Graphics g) {
-    g.fillArc(40, 30, 100, 70, 45, 135);
     // 外の枠
     g.setColor(new Color(255,255,255));
     g.drawRect(15,15,400,250);
+
     g.setColor(new Color(50,50,50));
     g.fillOval(15+80-1, 15+5-1, 242, 242);
     g.setColor(new Color(35,35,35));
@@ -32,9 +34,24 @@ public class PieGraph extends Applet {
     g.fillOval(15+120, 15+45, 160, 160);
     g.setColor(new Color(50,50,50));
     g.fillOval(15+140, 15+65, 120, 120);
+    //メーター
+    int i;
+    int arc1 = 90;
+    int length;
+    for(i=0;i<8;i++){
+      length = Math.round(360*pa[i]/sum);
+      g.setColor(new Color(0+i*10,0+i*15,0+i*35));
+      g.drawString(sum+"",15+40,30+15*i);
+      g.drawString(length+"",15+15,30+15*i);
+      g.fillArc(15+140, 15+65, 120, 120, arc1, length);
+      arc1 += length;
+    }
     g.setColor(new Color(35,35,35));
     g.fillOval(15+160, 15+85, 80, 80);
     g.setColor(new Color(50,50,50));
     g.fillOval(15+180, 15+105, 40, 40);
+    //円縦線
+    g.setColor(new Color(200,200,200));
+    g.fillRect(15+200-1,15+5,3,120);
   }
 }
