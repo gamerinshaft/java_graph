0502d5c9c2f066a01f1a13bc9e9360144007335b
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-29 22:32:36 +0900

    選が表示されるようにした

--- LineGraph.java
+++ LineGraph.java
@@ -6,7 +6,7 @@ public class LineGraph extends Applet {
   float max = 0;
   public void init() {
      setBackground(new Color(25,25,25));
-     pa[0]=500;pa[1]=640;pa[2]=340;pa[3]=412;pa[4]=560;pa[5]=109;
+     pa[0]=700;pa[1]=640;pa[2]=340;pa[3]=412;pa[4]=560;pa[5]=109;
      pa[6]=800;pa[7]=250;
      //値の比較
      int i;
@@ -32,15 +32,35 @@ public class LineGraph extends Applet {
     //横線
     g.drawLine(15+45,250-15,390,250-15);
     //縦線データー線記入
-    g.setColor(new Color(255,255,255));
-    g.drawLine(15+45,250-20-(int)(205*pa[0]/max),15+45,250-15);
+    if(250-20-(int)(205*pa[0]/max) > 15+10+51*3){
+      GradientPaint gp = new GradientPaint(15+45,15+10+51*3,Color.RED,15+45,250-15,Color.GRAY);
+      g2.setPaint(gp);
+    }else if(250-20-(int)(205*pa[0]/max) > 15+10+51*3){
+      GradientPaint gp = new GradientPaint(15+45,15+10+51*3,Color.ORANGE,15+45,250-15,Color.RED);
+      g2.setPaint(gp);
+    }else if(250-20-(int)(205*pa[0]/max) > 15+10+51*2){
+      GradientPaint gp = new GradientPaint(15+45,15+10+51*2,Color.GREEN,15+45,250-15,Color.ORANGE);
+      g2.setPaint(gp);
+    }else if(250-20-(int)(205*pa[0]/max) > 15+10+51*1){
+      GradientPaint gp = new GradientPaint(15+45,15+10+51*1,Color.BLUE,15+45,250-15,Color.GREEN);
+      g2.setPaint(gp);
+    }else if(250-20-(int)(205*pa[0]/max) > 15+10+51*0){
+      GradientPaint gp = new GradientPaint(15+45,15+10+51*0,Color.CYAN,15+45,250-15,Color.BLUE);
+      g2.setPaint(gp);
+    }
+    g2.drawLine(15+45,250-20-(int)(205*pa[0]/max),15+45,250-15);
     //縦線数値の記入
     //縦丸ぽちの追加
    for(i=0;i<4;i++){
       g.setColor(new Color(255,255,255));
       g.drawString((int)(max*(4-i)/4)+"",15+10,15+15+5+51*i);
-      g.setColor(new Color(50,50,50));
-      g.fillOval(15+40,15+10+51*i,10,10);
+      if(250-20-(int)(205*pa[0]/max) < 15+10+51*i){
+        g.setColor(new Color(255,255,255));
+        g.fillOval(15+40,15+10+51*i,10,10);
+      }else{
+        g.setColor(new Color(50,50,50));
+        g.fillOval(15+40,15+10+51*i,10,10);
+      }
     }
     //横線数値記入
     //横丸ぽちの追加
@@ -54,7 +74,11 @@ public class LineGraph extends Applet {
     g.setColor(new Color(255,255,255));
     for(i=0;i<8;i++){
       g.fillOval((int)(15+40+335*i/7),250-20-(int)(205*pa[i]/max),10,10);
+      if(i==7){
+        break;
+      }else{
+        g.drawLine((int)(15+40+335*i/7)+5,5+250-20-(int)(205*pa[i]/max),5+(int)(15+40+335*(i+1)/7),5+250-20-(int)(205*pa[i+1]/max));
+      }
     }
   }
-
 }
