49aeb527e480eda2f56673f543351bd38dab7b0a
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-05-02 15:00:12 +0900

    レーダーの枠線ひょうじ

--- RaderChart.java
+++ RaderChart.java
@@ -2,7 +2,7 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class RaderChart extends Applet {
-  float pa[]=new float[12];
+  float pa[]=new float[180];
   float max = 0;
   float sum = 0;
   public void init() {
@@ -27,15 +27,28 @@ public class RaderChart extends Applet {
     double rad = (360/pa.length)*(Math.PI/180);
     int i;
     for(i=0;i<pa.length;i++){
-      int x1= (int)(100*Math.cos(90*Math.PI/180+rad*i));
-      int y1= (int)(100*Math.sin(90*Math.PI/180+rad*i));
-      g.drawLine(215,140,215+x1,140-y1);
+      int x1[] = new int[4];
+      int y1[] = new int[4];
+      int k;
+      for(k=0;k<4;k++){
+        x1[k]= (int)((100-25*k)*Math.cos(90*Math.PI/180+rad*i));
+        y1[k]= (int)((100-25*k)*Math.sin(90*Math.PI/180+rad*i));
+      }
+      g.drawLine(215,140,215+x1[0],140-y1[0]);
       if(i+1<pa.length){
-        int x2= (int)(100*Math.cos(90*Math.PI/180+rad*(i+1)));
-        int y2= (int)(100*Math.sin(90*Math.PI/180+rad*(i+1)));
-        g.drawLine(215+x2,140-y2,215+x1,140-y1);
+        int x2[] = new int[4];
+        int y2[] = new int[4];
+        int j;
+        for(j=0;j<4;j++){
+          x2[j]= (int)((100-25*j)*Math.cos(90*Math.PI/180+rad*(i+1)));
+          y2[j]= (int)((100-25*j)*Math.sin(90*Math.PI/180+rad*(i+1)));
+          g.drawLine(215+x2[j],140-y2[j],215+x1[j],140-y1[j]);
+        }
+
       }else{
-        g.drawLine(215,40,215+x1,140-y1);
+        for(k=0;k<4;k++){
+          g.drawLine(215,40+25*k,215+x1[k],140-y1[k]);
+        }
       }
     }
 
